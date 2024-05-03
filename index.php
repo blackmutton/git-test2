cac2680f094b6e68da05bbacc8e2b6a5ec239c97
yt Chen <yutongc30@gmail.com>
2024-05-03 10:43:23 +0800

    畫了一個表格

--- index.php
+++ index.php
@@ -5,8 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>github 操作練習</title>
+    <style>
+        table{
+            border-collapse:collapse;
+        }
+        td{
+            border:1px black solid;
+            padding:0 25px;
+        }
+    </style>
 </head>
 <body>
-    測試測試
+    <h2>忘記push</h2>
+    <table>
+        <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>8</td>
+            <td>9</td>
+        </tr>
+    </table>
 </body>
 </html>
